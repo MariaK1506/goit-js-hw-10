f1c7e33c3e8eebff6c3c2ee6f427e54cc7b921aa
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-06-06 21:03:13 +0300

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,10 +4,11 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 function fetchCountries(name) {
   return fetch(
     `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  ).then(
+    response => response.json()
+    // if (!response.ok) {
+    //   throw new Error(response.status);
+    // }
+    // return response.json();
+  );
 }
